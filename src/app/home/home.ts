ca6f1db3db9486895c1dd5c21e9b295cb1894a3c
zeyad salim <ziadsalim121@gmail.com>
2026-03-13 01:13:48 +0200

    feat: Add initial e-commerce structure including product list, home page, and header components.

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,17 +1,19 @@
 import { ProductService } from '@/services/product.service';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { TitleCasePipe, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink,UpperCasePipe,TitleCasePipe],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home implements OnInit {
-private productservice=inject(ProductService);
-product = this.productservice.getallproducts;
-ngOnInit(): void {
-  this.productservice.getallproducts();
-}
+  private productservice = inject(ProductService);
+  products = this.productservice.allProducts;
+  product = this.productservice.getallproducts;
+  ngOnInit(): void {
+    this.productservice.getallproducts();
+  }
 }
