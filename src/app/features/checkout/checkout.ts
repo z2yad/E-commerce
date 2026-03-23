ee4a6b8bfc50eced929fd6d2349bafb776550436
zeyad salim <ziadsalim121@gmail.com>
2026-03-23 05:16:02 +0200

    feat(payment): implement dynamic credit card values using two-way binding
    - Added dynamic data binding for credit card inputs
    - Implemented two-way binding for real-time UI updates
    - Synced card number, holder name, expiry date, and CVV with UI
    - Improved user interaction and form experience

--- src/app/features/checkout/checkout.ts
+++ src/app/features/checkout/checkout.ts
@@ -1,12 +1,13 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CommonModule, DecimalPipe } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
 import { CartService } from '@/services/cart.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
   standalone: true,
-  imports: [CommonModule, DecimalPipe, RouterLink],
+  imports: [CommonModule, DecimalPipe, RouterLink, FormsModule],
   templateUrl: './checkout.html',
   styleUrl: './checkout.css',
 })
@@ -17,5 +18,22 @@ export class Checkout {
   totalPrice = this.cartService.totalPrice;
   countItemsScroll = this.cartService.countItems;
 
+  // two way binding with card and input
+  name = '';
+  cardnumber = '';
+  cardexpiry = '';
+  cardcvv = '';
+
+  get formattedCardNumber() {
+    if (!this.cardnumber) return '4242 •••• •••• 1337';
+    // Format: 4 digits then space
+    const cleaned = this.cardnumber.replace(/\D/g, '');
+    const groups = cleaned.match(/.{1,4}/g);
+    return groups ? groups.join(' ') : cleaned;
+  }
+
+  get formattedExpiry() {
+    return this.cardexpiry || '12/28';
+  }
 
 }
