a695d85bb2e776e0ca563821aa871beb296c14a2
zeyad salim <ziadsalim121@gmail.com>
2026-05-06 02:44:42 +0300

    add admin-layout

---
+++ rendering/src/app/features/admin/admin-layout/admin-layout.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-admin-layout',
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  templateUrl: './admin-layout.html',
+})
+export class AdminLayout {}
