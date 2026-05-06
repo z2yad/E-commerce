6cef522867e0286264d0dc1fdbc6c7bab0bde1fb
zeyad salim <ziadsalim121@gmail.com>
2026-05-06 03:04:26 +0300

    update wuth ui when the user login if admin navbar will be hidden else apper when is user

--- rendering/src/app/app.ts
+++ rendering/src/app/app.ts
@@ -1,14 +1,25 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { filter, map, startWith } from 'rxjs';
 import { Header } from "./shared/components/header/header";
 import { ZardToastComponent } from './shared/components/toast';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header,ZardToastComponent],
+  imports: [RouterOutlet, Header, ZardToastComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('rendering');
+  private router = inject(Router);
+
+  isAdminRoute = toSignal(
+    this.router.events.pipe(
+      filter(e => e instanceof NavigationEnd),
+      map(e => e.url.startsWith('/admin')),
+      startWith(this.router.url.startsWith('/admin'))
+    ),
+    { initialValue: false }
+  );
 }
